1f4195c42439a7d0885dddcc55040f9410018c24
henrytsai45 <116875469+henrytsai45@users.noreply.github.com>
2025-02-08 14:59:04 +0800

    Update IHDOC_end.py

--- IHDOC_end.py
+++ IHDOC_end.py
@@ -2,6 +2,7 @@ import os
 import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
